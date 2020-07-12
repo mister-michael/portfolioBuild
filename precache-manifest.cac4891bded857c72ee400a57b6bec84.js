d81c2f979712110b44d7e9619ac27923d8b10f3c
Michael Clark <michaelclarknashville@protonmail.com>
2020-07-11 23:59:21 -0500

    initial commit for build

---
+++ precache-manifest.cac4891bded857c72ee400a57b6bec84.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3591939dee07dd8bf16116c54e0f0045",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b624c063f5ae661fe6b5",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "a5e0065865b0d0ccb30d",
+    "url": "/static/css/main.db41df49.chunk.css"
+  },
+  {
+    "revision": "b624c063f5ae661fe6b5",
+    "url": "/static/js/2.af3f9aeb.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/static/js/2.af3f9aeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5e0065865b0d0ccb30d",
+    "url": "/static/js/main.9042b422.chunk.js"
+  },
+  {
+    "revision": "30ef98ccc078dcdc8699",
+    "url": "/static/js/runtime-main.13135219.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "884bf48f1b14a1357830782b7717af69",
+    "url": "/static/media/threeMikes-big.884bf48f.jpg"
+  },
+  {
+    "revision": "2ab1b619cba735adef747e8449392c1b",
+    "url": "/static/media/threeMikes-left-focus.2ab1b619.jpg"
+  },
+  {
+    "revision": "ab73eb472f78cb516fc2510ec0b79ace",
+    "url": "/static/media/threeMikes-right-focus.ab73eb47.jpg"
+  },
+  {
+    "revision": "c6e9d0ae8fa42aa84c524fcd32fa452a",
+    "url": "/static/media/threeMikes.c6e9d0ae.jpg"
+  }
+]);
